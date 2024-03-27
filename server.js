f1d497cb24a2d8c2c4804641c7eecda6cc543131
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-27 16:45:50 -0700

    Setup server.js
    - Initialized express app
    - Setup session middleware for user auth
    - Configured Handlebars.js as the view engine
    - Setup middle for parsing data and serving static files
    - Defined routes using the import path module
    - Synced modles with the db and start the server

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT;
+
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: true,
+};
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}.`));
+});
