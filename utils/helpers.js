5d0bf81a703a3ca90adb0c27905f4eba01699104
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-04-01 18:35:18 -0700

    added helper to truncate text and show a ... with a read me link, this will prevent blogs showing the entire content on the homapage

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,4 +4,11 @@ module.exports = {
   format_date: (date) => {
     return moment(date).fromNow();
   },
+  truncate: (str, len) => {
+    if (str.length > len) {
+      return str.substring(0, len) + '...';
+    } else {
+      return str;
+    }
+  }
 };
